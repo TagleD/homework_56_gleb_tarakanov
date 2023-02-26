354a2cfacc17e4e304c86f47bcac4c79782fa5a6
GLeb Tarakanov <glebtgd@gmail.com>
2023-02-26 21:23:36 +0600

    Созданы страницы просмотра товара:     Список товаров (главная страница).     Просмотр одного товара по id. Добавлен базовый шаблон и меню навигации. Отсортированы товары на главной по категориям и названиям в алфавитном порядке.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 
 
-from webapp.views.products import products_view
+from webapp.views.products import products_view, product_view
 
 
 urlpatterns = [
     path('', products_view, name='index'),
-    # path('products', products_view, name='products_view'),
-    # path('products/<int:pk>', product_view, name='product_detail'),
+    path('products', products_view, name='products_view'),
+    path('products/<int:pk>', product_view, name='product_detail'),
     # path('products/add', products_add_view, name='products_add'),
     # path('products/<int:pk>/delete', products_delete_view, name='product_delete'),
     # path('products/<int:pk>/edit', products_edit_view, name='product_edit'),
--- source/webapp/views/products.py
+++ source/webapp/views/products.py
@@ -3,15 +3,17 @@ from webapp.models import Product
 
 
 def products_view(request):
-    products = Product.objects.all()
+    products = Product.objects.exclude(balance=0).order_by(
+        'category', 'name'
+    )
     context = {'products': products}
     return render(request, 'products.html', context=context)
 
 
-# def product_view(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {'product': product}
-#     return render(request, 'product.html', context=context)
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product.html', context=context)
 
 
 # def products_add_view(request):
